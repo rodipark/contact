86c050d862317897f6bb90c43ae714906e0e92be
rodi <progint21@gmail.com>
2014-11-05 09:22:02 +0000

    GET 방식으로 파라미터 받기 - 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Contact::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  
+	get 'email/email_form' => 'email#email_form'
+	get 'email/email_send' => 'email#email_send'
+
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
